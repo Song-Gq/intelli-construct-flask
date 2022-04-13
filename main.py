7bc38ab881ab566e5ae801df1b2918c38fb67c32
Song-Gq <guanqunsong@qq.com>
2022-04-13 20:24:09 +0800

    New features added according to UI. Added token mechanism; Enabled multi-client access; Enabled Excel file export and files management; Updated old index() method to fuzz_index();

--- main.py
+++ main.py
@@ -1,10 +1,87 @@
+import json
+import os
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from start_recognition import start_recognition, get_prog
+from start_recognition import start_recognition, get_prog, get_excel
 import webbrowser
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 
 app = Flask(__name__, static_folder='./dist', static_url_path='/')
+app.config['SECRET_KEY'] = 'trustedailab'
 CORS(app)
+
+max_user_num = 200
+check_expired_user_interval = 20
+check_expired_user_counter = 0
+
+
+def check_expired_user():
+    global check_expired_user_counter
+    check_expired_user_counter = check_expired_user_counter + 1
+    if check_expired_user_counter >= check_expired_user_interval:
+        check_expired_user_counter = 0
+        with open('ids.json', 'r') as f:
+            j = json.load(f)
+            user_list = j[1]['user_list']
+            for key in user_list:
+                v_id = verify_auth_token(user_list[key])
+                if v_id is None:
+                    with open('ids.json', 'w') as f_w:
+                        user_list.pop(key)
+                        json.dump(j, f_w)
+                    os.remove("out_excel/{}.xlsx".format(key))
+                    print('user expired: {}'.format(key))
+                    print('tokens remaining: {}'.format(len(user_list)))
+                if v_id != key:
+                    print('user id and token not identical!\nv_id: {}\nu_id: {}\ntoken: {}'
+                          .format(v_id, key, user_list[key]))
+                    # raise Exception('user id and token not identical!\nv_id: {}\nu_id: {}\ntoken: {}'
+                    #                 .format(v_id, key, user_list[key]))
+
+
+def allocate_id():
+    with open('ids.json', 'r') as f:
+        j = json.load(f)
+    j[0]['new_id'] = j[0]['new_id'] + 1
+    if j[0]['new_id'] > 1000000:
+        j[0]['new_id'] = 1
+    with open('ids.json', 'w') as f:
+        json.dump(j, f)
+    return j[0]['new_id']
+
+
+# 获取token，有效时间1h
+def generate_auth_token(expiration=3600):
+    try:
+        check_expired_user()
+        with open('ids.json', 'r') as f:
+            j = json.load(f)
+            user_list = j[1]['user_list']
+            if len(user_list) < max_user_num:
+                user = allocate_id()
+                s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+                t = s.dumps({'id': user})
+                with open('ids.json', 'w') as f_w:
+                    user_list[user] = str(t)
+                    json.dump(j, f_w)
+                return t
+            return None
+    except Exception as e:
+        print("generate_auth_token(): {}".format(e))
+        return None
+
+
+# 解析token，确认用户身份
+def verify_auth_token(token):
+    s = Serializer(app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except SignatureExpired:
+        return None  # valid token, but expired
+    except BadSignature:
+        return None  # invalid token
+    return data['id']
 
 
 @app.route('/', methods=['GET'])
@@ -15,16 +92,65 @@ def index():
 @app.route('/api/recognition', methods=['POST'])
 def recognition():
     files = request.files.to_dict()
-    res, mis, query_id = start_recognition(files)
-    return jsonify({'res': res, 'mis': mis, 'query_id': query_id})
+    h = request.headers
+    t = h['token']
+    proc_id = verify_auth_token(t)
+    if proc_id is None:
+        return "token invalid!", 401
+    res, mis = start_recognition(files, proc_id)
+    if res is None:
+        return 'No legal screenshots!', 503
+    return jsonify({'res': res, 'mis': mis})
 
 
 @app.route('/api/getprog', methods=['GET'])
 def getprog():
     p = request.args.to_dict()
-    proc_id = p['proc_ID']
+    t = p['token']
+    proc_id = verify_auth_token(t)
+    if proc_id is None:
+        return jsonify(-1)
     prog = get_prog(proc_id)
     return jsonify(prog)
+
+
+@app.route('/api/gettoken', methods=['GET'])
+def gettoken():
+    t = generate_auth_token()
+    if t is not None:
+        return t
+    else:
+        return 'Too much users now', 503
+
+
+@app.route('/api/destroytoken', methods=['DELETE'])
+def destroytoken():
+    p = request.args.to_dict()
+    if len(p) == 0:
+        return 'user not created'
+    user = verify_auth_token(p['token'])
+    try:
+        with open('ids.json', 'w') as f:
+            j = json.load(f)
+            user_list = j[1]['user_list']
+            user_list.pop(user)
+            json.dump(j, f)
+    except Exception as e:
+        print(e)
+    return 'OK'
+
+
+@app.route('/api/getexcel', methods=['GET'])
+def getexcel():
+    p = request.args.to_dict()
+    t = p['token']
+    proc_id = verify_auth_token(t)
+    if proc_id is None:
+        return "token invalid!", 401
+    f = get_excel(proc_id)
+    if f is not None:
+        return f
+    return jsonify('File has been deleted!')
 
 
 @app.route('/<path:fallback>')
--- start_recognition.py
+++ start_recognition.py
@@ -5,10 +5,17 @@ import numpy as np
 import re
 import xlwt
 import datetime
+import time
+
+from flask import send_file
+
+fnum_dict = {}
+fdone_dict = {}
 
 
-fnum = 0
-fdone = 0
+def fuzz_index(in_list, target_str):
+    target_list = [i for i, x in enumerate(in_list) if x.find(target_str) != -1]
+    return target_list[0]
 
 
 def foldread(prepath: str):
@@ -28,102 +35,127 @@ def foldread(prepath: str):
     return foldname
 
 
+def format_time(t_str):
+    try:
+        t_str = t_str.replace('l', '1')
+        t_str = t_str.replace('/', '1')
+        t_str = t_str.replace('\\', '1')
+        t_str = t_str.replace('|', '1')
+        t_str = t_str.replace(" ", "")
+        t_str = t_str.replace(".", ":")
+        t_str = datetime.datetime.strptime(t_str, "%Y-%m-%d%X")
+        return t_str.strftime("%Y-%m-%d %X")
+    except Exception as e:
+        print(e)
+        return t_str
+
+
 # 从文件列表中读取图片,识别图片，[姓名，采样和检测，采样、检测结果，日期，来源？]
-def f_imgread(flist):
+def f_imgread(flist, proc_id):
     print('开始读取图片并识别')
     info = ['类型', '姓名', '采样时间', '检测结果']
     res = []
     mis = []
     reader = easyocr.Reader(['ch_sim', 'en'])  # this needs to run only once to load the model into memory
     resi = []
-    global fdone
     for f in flist:
-        resj = [f]
-        img = cv2.imdecode(np.asarray(bytearray(flist[f].read()), dtype="uint8"), cv2.IMREAD_COLOR)
-        img_array = np.array(img)
-        result = reader.readtext(img_array, detail=0)
-        iname = result.index('姓名')
-        if '\u4e00' <= result[iname + 1][0] <= '\u9fff':
-            iname += 1
-        else:
-            iname += 2
-        name = result[iname]
-        itime = result.index('采样时间')
-        time = result[itime + 1]
-        ire = result.index('检测结果')
-        re = result[ire + 1]
-        if '上传' in re:
-            f = '已采样'
-            c = '采样'
-        if '阴' in re:
-            f = '已检测' + re
-            c = '检测'
-        elif '阳' in re:
-            f = '已检测' + re
-            c = '检测'
-            mis.append([c, name, time, f])
-        resj += [c, name, time, f]
-        if len(resj) == 5:
-            resi.append(resj)
-        else:
-            mis.append(resj)
-        fdone = fdone + 1
-    res.append(resi)
-    return res, mis
-
-
-# 从文件夹列表中读取图片,识别图片，[姓名，采样和检测，采样、检测结果，日期，来源？]
-def imgread(foldpath: str):
-    print('开始读取图片并识别')
-    info = ['图片路径', '类型', '姓名', '采样时间', '检测结果']
-    m = len(foldpath)
-    res = []
-    mis = []
-    reader = easyocr.Reader(['ch_sim', 'en'])  # this needs to run only once to load the model into memory
-    for i in range(m):
-        fold = foldpath[i]
-        filelist = os.listdir(fold)
-        resi = []
-        for filename in filelist:
-            file = fold + './' + filename
-            resj = [file]
-            img = cv2.imdecode(np.fromfile(file, dtype=np.uint8), -1)
+        try:
+            resj = []
+            img = cv2.imdecode(np.asarray(bytearray(flist[f].read()), dtype="uint8"), cv2.IMREAD_COLOR)
             img_array = np.array(img)
             result = reader.readtext(img_array, detail=0)
-            iname = result.index('姓名')
+            iname = fuzz_index(result, '姓名')
+            # iname = result.index('姓名')
             if '\u4e00' <= result[iname + 1][0] <= '\u9fff':
                 iname += 1
             else:
                 iname += 2
             name = result[iname]
-            itime = result.index('采样时间')
-            time = result[itime + 1]
-            ire = result.index('检测结果')
-            re = result[ire + 1]
-            if '上传' in re:
+            itime = fuzz_index(result, '采样时间')
+            # itime = result.index('采样时间')
+            t = format_time(result[itime + 1])
+            ire = fuzz_index(result, '检测结果')
+            # ire = result.index('检测结果')
+            r = result[ire + 1]
+            c = ""
+            if '上传' in r:
                 f = '已采样'
                 c = '采样'
-            if '阴' in re:
-                f = '已检测' + re
+            if '阴' in r:
+                f = '已检测' + r
                 c = '检测'
-            elif '阳' in re:
-                f = '已检测' + re
+            elif '阳' in r:
+                f = '已检测' + r
                 c = '检测'
-                mis.append([fold, c, name, time, f])
-            resj += [c, name, time, f]
-            if len(resj) == 5:
+                mis.append([c, name, t, f])
+            resj = [c, name, t, f]
+            if len(resj) == 4:
                 resi.append(resj)
             else:
                 mis.append(resj)
-        res.append(resi)
+            fdone_dict[proc_id] = fdone_dict[proc_id] + 1
+        except Exception as e:
+            print("f_imgread(): {}\nfile: {}".format(e, f))
+            fname = f.split('=')[2]
+            misf = ["未知图片", fname, " ", "检测失败"]
+            mis.append(misf)
+            resi.append(misf)
+    res.append(resi)
     return res, mis
 
 
+# 从文件夹列表中读取图片,识别图片，[姓名，采样和检测，采样、检测结果，日期，来源？]
+# def imgread(foldpath: str):
+#     print('开始读取图片并识别')
+#     info = ['图片路径', '类型', '姓名', '采样时间', '检测结果']
+#     m = len(foldpath)
+#     res = []
+#     mis = []
+#     reader = easyocr.Reader(['ch_sim', 'en'])  # this needs to run only once to load the model into memory
+#     for i in range(m):
+#         fold = foldpath[i]
+#         filelist = os.listdir(fold)
+#         resi = []
+#         for filename in filelist:
+#             file = fold + './' + filename
+#             resj = [file]
+#             img = cv2.imdecode(np.fromfile(file, dtype=np.uint8), -1)
+#             img_array = np.array(img)
+#             result = reader.readtext(img_array, detail=0)
+#             iname = result.index('姓名')
+#             if '\u4e00' <= result[iname + 1][0] <= '\u9fff':
+#                 iname += 1
+#             else:
+#                 iname += 2
+#             name = result[iname]
+#             itime = result.index('采样时间')
+#             time = result[itime + 1]
+#             ire = result.index('检测结果')
+#             re = result[ire + 1]
+#             if '上传' in re:
+#                 f = '已采样'
+#                 c = '采样'
+#             if '阴' in re:
+#                 f = '已检测' + re
+#                 c = '检测'
+#             elif '阳' in re:
+#                 f = '已检测' + re
+#                 c = '检测'
+#                 mis.append([fold, c, name, time, f])
+#             resj += [c, name, time, f]
+#             if len(resj) == 5:
+#                 resi.append(resj)
+#             else:
+#                 mis.append(resj)
+#         res.append(resi)
+#     return res, mis
+
+
 # #输出为EXCEL
-def Toxls(res, mis, info):
+def Toxls(res, mis, info, proc_id):
     print('开始生成检测统计报告')
     workbook = xlwt.Workbook()
-    sheet = workbook.add_sheet("Sheet Name1")
+    sheet = workbook.add_sheet("Sheet1")
     for i in range(len(info)):
         sheet.write(0, i, info[i])
     c = 1
@@ -133,34 +165,51 @@ def Toxls(res, mis, info):
                 sheet.write(c, j, per[j])
             c += 1
 
-    curr_time = datetime.date.today()
-    excel_filename = str(curr_time) + '核酸采样结果' + '.xls'
-    workbook.save(excel_filename)
-    print('生成检测报告为' + excel_filename)
+    # curr_time = datetime.datetime.now().strftime("%Y-%m-%d %X")
+    excel_filename = str(proc_id)
+    workbook.save("out_excel/{}.xlsx".format(excel_filename))
+    print("生成检测报告: out_excel/{}.xlsx".format(excel_filename))
 
 
-def start_recognition(files):
+def start_recognition(files, proc_id):
     # path = 'imgs'
     # foldpath = foldread(path)
     # res, mis = imgread(foldpath)
-    global fnum
-    global fdone
-    fnum = len(files)
-    fdone = 0
+    fnum_dict[proc_id] = len(files)
+    fdone_dict[proc_id] = 0
     try:
-        res, mis = f_imgread(files)
+        res, mis = f_imgread(files, proc_id)
         if mis:
             print('存在异常检测结果', mis)
-        info = ['图片路径', '类型', '姓名', '采样时间', '检测结果']
-        Toxls(res, mis, info)
-        return res[0], mis, info
+        info = ['类型', '姓名', '采样时间', '检测结果']
+        Toxls(res, mis, info, proc_id)
+        fnum_dict.pop(proc_id)
+        fdone_dict.pop(proc_id)
+        return res[0], mis
     except Exception as e:
-        print(e)
-        return None, None, None
+        print("start_recognition(): {}".format(e))
+        fnum_dict.pop(proc_id)
+        fdone_dict.pop(proc_id)
+        return None, None
 
 
 def get_prog(proc_id):
-    return fdone / fnum
+    try:
+        if fnum_dict[proc_id] == 0:
+            return -1
+        return fdone_dict[proc_id] / fnum_dict[proc_id]
+    except Exception as e:
+        print("get_prog() in start_recognition.py: {}".format(e))
+        return -1
+
+
+def get_excel(proc_id):
+    file_path = "out_excel/{}.xlsx".format(proc_id)
+    if os.path.exists(file_path):
+        curr_time = datetime.datetime.now().strftime("%Y-%m-%d %X")
+        return send_file(file_path, as_attachment=True,
+                         attachment_filename="核酸检测识别报告-{}.xlsx".format(curr_time))
+    return None
 
 
 # if __name__ == '__main__':
